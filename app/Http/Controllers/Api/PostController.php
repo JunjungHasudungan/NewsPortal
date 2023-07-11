0f0648c963b6f9cd2a934d79fa1c39aa0b07e79c
junjunghasudungan <junjunghasudungan43@gmail.com>
2023-07-11 09:30:12 +0700

    adding updatePostRequest in PostController

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\Post;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\{
+    PostRequest,
+    UpdatePostRequest
+};
 use App\Http\Resources\PostResource;
 use Illuminate\Http\Request;
 
@@ -28,9 +32,13 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(PostRequest $request)
     {
-        //
+        // membuat data post melalui var request model binding
+        Post::create($request->validated());
+
+        // memberi respon json
+        return response()->json('data post created..');
     }
 
     /**
@@ -52,9 +60,11 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Post $post)
+    public function update(UpdatePostRequest $request, Post $post)
     {
-        //
+        $post->update($request->validated());
+
+        return response()->json('data post updated..');
     }
 
     /**
